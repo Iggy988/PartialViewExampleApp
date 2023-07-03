ce5693351a7c5002fc6b0e519a0f344a0df34795
Iggy988 <igor.rodic1988@gmail.com>
2023-07-03 22:53:22 +0200

    Add project files.

---
+++ PartialViewExample/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewExample.Models;
+
+namespace PartialViewExample.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        //ViewData["ListTitle"] = "Cities";
+        //ViewData["ListItems"] = new List<string>()
+        //{
+        //    "Paris",
+        //    "Rome",
+        //    "Belgrade",
+        //    "New York"
+        //};
+        return View();
+    }
+
+    [Route("about")]
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    [Route("programming-languages")]
+    public IActionResult ProgrammingLanguages()
+    {
+        ListModel listModel = new()
+        {
+            ListTitles = "",
+            ListItems = new List<string>()
+            {
+                "C#",
+                "C++",
+                "Java",
+                "Dart",
+                "Pascal"
+            }
+        };
+        //return new PartialViewResult(){ViewName = "_ListPartialView", Model = listModel}; 
+        return  PartialView("_ListPartialView", listModel); 
+    }
+}
---
+++ PartialViewExample/Models/ListModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PartialViewExample.Models;
+
+public class ListModel
+{
+    public string ListTitles { get; set; } = "";
+    public List<string> ListItems { get; set; } = new List<string>();
+}
